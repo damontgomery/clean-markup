4d8e4680d7b366288ed84720b5cc885130dcc77b
Daniel Montgomery <damontgomery@gmail.com>
2014-02-15 13:05:21 -0800

    version 1.0

--- docroot/sites/all/themes/pandatheme/templates/node.tpl.php
+++ docroot/sites/all/themes/pandatheme/templates/node.tpl.php
@@ -1,18 +1,13 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
   <?php print render($title_prefix); ?>
   <?php if (isset($title)): ?>
     <h1<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
   <?php if (isset($unpublished)): ?>
     <div class="unpublished"><?php print t('Unpublished'); ?></div>
   <?php endif; ?>
-
   <div class="content"<?php print $content_attributes; ?>>
     <?php print render($content);?>
   </div>
-
-  <?php // print render($content['links']); ?>
-</div><!-- /.node -->
+</div>
